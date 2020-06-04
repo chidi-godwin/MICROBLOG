cf7f58c8eda6b8c7cf58ad3ab3f2f7e363d6e7ab
gnc <chidieberen1998@gmail.com>
2020-06-04 22:58:32 +0100

    Implement user login

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
@@ -25,4 +27,8 @@ class Post(db.Model):
     user_id = db.Column(db.Integer,  db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return f"<Post {self.body}>s"
+        return f"<Post {self.body}>"
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
