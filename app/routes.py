ab419755b4234b418ab3fdaeb09b0fa410e223c8
gnc <chidieberen1998@gmail.com>
2020-06-05 19:33:59 +0100

    debug errors in occured in registration and login

--- app/routes.py
+++ app/routes.py
@@ -36,7 +36,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        return redirect(url_for('/index'))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
