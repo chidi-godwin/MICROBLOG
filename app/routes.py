fcf54f3502048d00ec2e3861eb37ad8ebe041f5a
gnc <chidieberen1998@gmail.com>
2020-07-21 05:11:53 +0100

    update: add user profile page

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from app.models import User
 from werkzeug.urls import url_parse
 from app import db
 
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -21,7 +22,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home Page', posts=posts)
-    
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -39,10 +41,12 @@ def login():
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -57,3 +61,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
