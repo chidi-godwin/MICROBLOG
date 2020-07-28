044384e65627b0fc2f536c4a865029aa47e166ce
gnc <chidieberen1998@gmail.com>
2020-07-28 23:58:56 +0100

    update: implemented mailing with flsk mail and password reset

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from app import db
+import jwt
+from time import time
+from app import app, db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -59,6 +61,21 @@ class User(UserMixin, db.Model):
         return followed.union(own).order_by(
             Post.timestamp.desc()
         )
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 
     def __repr__(self):
         return f"<User {self.username}>"
